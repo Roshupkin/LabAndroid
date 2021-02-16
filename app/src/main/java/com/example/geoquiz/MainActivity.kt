9a23e3e6b874375c698941211919acb7e6898048
Chisster <max17121999@yandex.ru>
2021-02-16 16:22:50 +0300

    Lab 2

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.geoquiz.databinding.ActivityMainBinding
 import java.lang.Exception
@@ -14,8 +15,18 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_4, true),
         Question(R.string.question_5, false)
     )
+private val TAG = "Main Activity"
+    private var indexQuestion: Int = 0
+        set(value) {
+            if (value < 0) {
+                field = question.size - 1
+            }else{
+                field = value
+            }
 
-    private var indexQuestion = 0
+
+        }
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,8 +42,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.buttonNext.setOnClickListener {
-            indexQuestion = (indexQuestion + 1) % question.size
-            binding.textQuestion.setText(question[indexQuestion].textResId)
+            nextQuestion()
+        }
+        binding.textQuestion.setOnClickListener {
+            nextQuestion()
+        }
+        binding.buttonBefore.setOnClickListener {
+            beforeQuestion()
         }
 
     }
@@ -46,6 +62,18 @@ class MainActivity : AppCompatActivity() {
             messageResId = R.string.incorrect_toast;
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    private fun nextQuestion() {
+        indexQuestion = (indexQuestion + 1) % question.size
+        binding.textQuestion.setText(question[indexQuestion].textResId)
+        Log.e(TAG, "Размер массива: ${question.size}, индекс вопроса в массиве: ${indexQuestion}")
+    }
+
+    private fun beforeQuestion() {
+        indexQuestion = (indexQuestion - 1) % question.size
+        binding.textQuestion.setText(question[indexQuestion].textResId)
+        Log.e(TAG, "Размер массива: ${question.size}, индекс вопроса в массиве: ${indexQuestion}")
     }
 
 }
